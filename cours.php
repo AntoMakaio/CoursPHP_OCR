1552f14215b767a7a4a414172bc696c577c72530
Makaio <antonin.begue@live.fr>
2017-02-18 23:08:20 +0100

    création de info.php + cours sur echo/date..

--- cours.php
+++ cours.php
@@ -1,4 +1,26 @@
-<?php 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Ma page web il est <?php echo date('h:i:s') ?></title>
+	</head>
+	<body>
+		<h1>Ma page web</h1>
+		<p>aujourd'hui nous somme le <?php echo date('d/m/Y h:i:s'); ?></p>
+		<?php 
+echo "Ceci est du <strong>texte</strong> \"uniquement\" en PHP " ;
 
-echo '<h1>hello à tous et à toutes !</h1>'
-?>
+	//commentaire sur une ligne
+	/*commentaire
+	sur plusieurs
+	ligne
+	*/
+	echo "1ere instruction ";
+	echo "2e instruction <br />";
+	echo "3e instruction";
+
+		phpinfo()
+		?>
+	</body>
+
+</html>
