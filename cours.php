b7f9325656fd789695e0a3d64936bfdb8c66c4fd
Makaio <antonin.begue@live.fr>
2017-02-20 20:29:16 +0100

    fin cours variable

--- cours.php
+++ cours.php
@@ -17,8 +17,26 @@ echo "Ceci est du <strong>texte</strong> \"uniquement\" en PHP " ;
 	*/
 	echo "1ere instruction ";
 	echo "2e instruction <br />";
-	echo "3e instruction";
+	echo "3e instruction<br /><br />";
+		?>
 
+		<?php 
+		//type int
+		$age_du_visiteur=17 ;
+		//type float
+		$poid = 45.5 ;
+		//type booleen
+		$je_suis_un_zero = false ;
+		$je_suis_bon = true ;
+		//variable vide avec NULL
+		$pas_de_valeur = NULL ;
+
+		echo 'Le visiteur à ' . $age_du_visiteur . ' ans' ;
+
+		$nombre = 10 ;
+		$resultat = ($nombre + 5) * $nombre ;
+
+		echo 'résultat du calcul est ' . $resultat . '';
 
 		?>
 	</body>
